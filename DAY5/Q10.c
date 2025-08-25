356ba7e3ca17fb2e316eb2b628c431c69f233e52
Dhruv-Pratap-Chauhan <dhruvpratapchauhan7@gmail.com>
2025-08-25 22:21:07 +0530

    day5 done
    ok

---
+++ DAY5/Q10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int sec, h, m, s;
+    printf("Enter time in seconds: ");
+    scanf("%d", &sec);
+    h = sec / 3600;
+    m = (sec % 3600) / 60;
+    s = sec % 60;
+    printf("Time: %d hours, %d minutes, %d seconds\n", h, m, s);
+    return 0;   
+}
