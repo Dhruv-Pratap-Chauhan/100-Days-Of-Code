a8c54de0e725bb2de798fc3939495bffc96953e5
Dhruv-Pratap-Chauhan <dhruvpratapchauhan7@gmail.com>
2025-11-09 12:14:01 +0530

    ok

---
+++ DAY23/Q45.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    float sum = 0.0;
+    int denom = 3;
+    for (int i = 1; i <= n; i++) {
+        sum += (float)(2 * i) / denom;
+        denom += 4;
+    }
+    printf("Approximate sum: %.2f\n", sum);
+    return 0;
+}
