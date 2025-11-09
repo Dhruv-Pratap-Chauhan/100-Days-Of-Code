62231c4a73a01091e8043757a3dd4a0b2c386e6d
Dhruv-Pratap-Chauhan <dhruvpratapchauhan7@gmail.com>
2025-11-09 13:39:35 +0530

    ok

--- DAY49/Q99.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char date[20];
-    printf("Enter date (dd/04/yyyy): ");
-    scanf("%s", date);
-    printf("Formatted date: %.2s-Apr-%4s\n", date, date+6);
-    return 0;
-}
